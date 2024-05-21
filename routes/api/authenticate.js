509939bcb46d07a8f29a67a353256fcd34d001c4
Sathvik Bennihalli <sathvikbennihalli@Sathviks-MacBook-Air.local>
2024-05-21 12:45:59 +0530

    spelling

--- routes/api/authenticate.js
+++ routes/api/authenticate.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { authenticate } from "../../controllers/authController.js";
-import verifyUser from "../../middleware/authMiddleware.js";
+import verifyUser from "../../middleware/authMiddleWare.js";
 const router = express.Router();
 
 router.get("/", verifyUser, authenticate);
