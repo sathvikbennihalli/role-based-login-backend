592728a40d6fc2fef8b04d5c95b456a10e9f0206
Sathvik Bennihalli <sathvikbennihalli@Sathviks-MacBook-Air.local>
2024-05-21 13:16:54 +0530

    views folder path update

--- routes/api/root.js
+++ routes/api/root.js
@@ -7,7 +7,7 @@ const __dirname = path.dirname(__filename);
 const router = express.Router();
 
 router.get("^/$|/index(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "..", "views", "index.html"));
 });
 
 export default router;
