8e8876773aa0a2f081e016d4c1b285cb82e62f9b
Sathvik Bennihalli <sathvikbennihalli@Sathviks-MacBook-Air.local>
2024-05-25 12:49:14 +0530

    authetication

--- middleware/authMiddleWare.js
+++ middleware/authMiddleWare.js
@@ -2,11 +2,13 @@ import jwt from "jsonwebtoken";
 
 const verifyUser = (req, res, next) => {
   const token = req.cookies.token;
+  console.log("Token:", token); // Log the token to verify its presence
   if (!token) {
     return res.json({ Error: "You are not authenticated" });
   }
   jwt.verify(token, "jwt-secret-key", (err, decoded) => {
     if (err) {
+      console.log("Token verification error:", err); // Log the error for debugging
       return res.json({ Error: "Token is not valid" });
     }
     req.user = decoded.user;
--- routes/api/authenticate.js
+++ routes/api/authenticate.js
@@ -3,6 +3,10 @@ import { authenticate } from "../../controllers/authController.js";
 import verifyUser from "../../middleware/authMiddleWare.js";
 const router = express.Router();
 
-router.get("/", verifyUser, authenticate);
+// router.get("/", verifyUser, authenticate);
+router.get("/", verifyUser, (req, res) => {
+  console.log("Authenticated user:", req.user); // Log the authenticated user
+  authenticate(req, res);
+});
 
 export default router;
